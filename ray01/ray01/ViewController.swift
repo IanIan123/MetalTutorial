9f041c438bd181d6a39b407092d14c5d6d06bfba
Ian <ian.warburton3000@gmail.com>
2024-01-06 17:30:01 +0000

    Init

---
+++ ray01/ray01/ViewController.swift
@@ -0,0 +1,49 @@
+import UIKit
+import MetalKit
+
+enum Colors {
+    static let wenderlichGreen = MTLClearColor(red: 1.0, green: 0.0, blue: 0.0, alpha: 1.0)
+}
+
+class ViewController: UIViewController {
+
+    var metalView: MTKView {
+        return view as! MTKView
+    }
+    
+    var device: MTLDevice!
+    var commandQueue: MTLCommandQueue!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        metalView.device = MTLCreateSystemDefaultDevice()
+        device = metalView.device
+        
+        metalView.clearColor = Colors.wenderlichGreen
+        metalView.delegate = self
+        
+        commandQueue = device.makeCommandQueue()
+    }
+
+
+}
+
+extension ViewController: MTKViewDelegate {
+    func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) { }
+    
+    func draw(in view: MTKView) {
+        guard let drawable = view.currentDrawable,
+              let descriptor = view.currentRenderPassDescriptor else {
+            return
+        }
+        let commandBuffer = commandQueue.makeCommandBuffer()
+        
+        let commandEncoder = commandBuffer?.makeRenderCommandEncoder(descriptor: descriptor)
+        
+        commandEncoder?.endEncoding()
+        commandBuffer?.present(drawable)
+        commandBuffer?.commit()
+        
+        
+    }
+}
